3f422c43cb2e301a69ba44e3fcfa8a1f97c8218c
Honcharuk Olena <gasmin07@gmail.com>
2022-12-01 09:35:33 +0200

    feat: change location to conroe

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -5,14 +5,14 @@ import './Map.css';
 export default function MapHouston() {
   return (
     <div className='maps'>
-      <h2>Houston-Balta</h2>
+      <h2>Conroe-Balta</h2>
       <div className='globe'>
         <img src={`${process.env.PUBLIC_URL}/globe.jpg`} alt="globe" />
       </div>
-      <Time/>
+      <Time />
       <div className="map-wrapper">
 
-      <iframe title="myFrame" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2905885.145531841!2d-96.91470895298819!3d31.267417925303103!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640b8b4488d8501%3A0xca0d02def365053b!2sHouston%2C%20TX%2C%20USA!5e0!3m2!1sen!2sua!4v1669800598860!5m2!1sen!2sua"
+      <iframe title="myFrame" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d220439.96479167434!2d-95.66389249961956!3d30.311865243153736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86472491556d3b49%3A0xe6352b9b1dad110e!2sConroe%2C%20TX%2C%20USA!5e0!3m2!1sen!2sua!4v1669878941739!5m2!1sen!2sua"
       style={{width: 400, height: 400, border: 0 }} loading="lazy"  referrerPolicy="no-referrer-when-downgrade" ></iframe>
       <iframe title="myFrame" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d42764.90718885823!2d29.586997173153296!3d47.94012267654934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40cc2f261757eb55%3A0x1d9b4421f6bed414!2sBalta%2C%20Odessa%20Oblast%2C%2066100!5e0!3m2!1sen!2sua!4v1669803602756!5m2!1sen!2sua"
       style={{width: 400, height: 400, border: 0 }} loading="lazy"  referrerPolicy="no-referrer-when-downgrade" ></iframe>
@@ -21,4 +21,5 @@ export default function MapHouston() {
     </div>
   );
 }
+
 
--- src/Time/Time.tsx
+++ src/Time/Time.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Time.css';
 
-export const Time = () => {
+export const Time = () =>  {
   const timeDavid = new Date().toLocaleString('en-US', { timeZone: 'America/Chicago' });
   const timeOlena = new Date().toLocaleString('en-US', { timeZone: 'Europe/Kiev' });
 
   return (
     <div className='time-wrapper'>
-      <div>David's time: {timeDavid}</div>
+      <div>David's time: {setTimeout(timeDavid, 1000)}</div>
       <div>Olena's time: {timeOlena}</div>
     </div>
-  );
-};
+  )};
+
+
+
